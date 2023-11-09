4cfd35c756ac84afb4f3018985f1df836add76e0
mf2056 <mf2056@hw.ac.uk>
2023-11-09 14:41:04 +0400

    added comments

--- week4/app.js
+++ week4/app.js
@@ -6,14 +6,16 @@ const info = document.getElementById('weather-info');
 button.addEventListener('click', function(){
     const api = 'd0a8acdd23e7995e921ab1c49957c17d';
     const cityname = input.value;
+// alert user if button is clicked with no input
     if (!cityname){
         alert("Please Enter a City name");
         return;
     }
-
+// creating the httpRequest to access info from openweathermap
     var ourRequest = new XMLHttpRequest();
     ourRequest.open('GET', `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${api}`);
 
+// Error messages printed for different cases
     ourRequest.onload = function() {
         if (ourRequest.status >= 200 && ourRequest.status < 400) {
             var data = JSON.parse(ourRequest.responseText);
@@ -38,11 +40,12 @@ button.addEventListener('click', function(){
 
 });
 
+// Displaying the output message
 function renderWeather(Data) {
-    var temp = (Data.main.temp - 273.15).toFixed(2);
+    var temp = (Data.main.temp - 273.15).toFixed(2); //temp changed from kelvin to celcius and displaying only two decimal spaces.
     var HTMLmessage = "<p>The weather in " + Data.name + " is " + Data.weather[0].description +
                       ".</br> The temperature is " + temp + "°C with a wind speed of " +
                       Data.wind.speed + "m/s. <hr></p>";
-    info.insertAdjacentHTML('beforeend', HTMLmessage);
+    info.insertAdjacentHTML('beforeend', HTMLmessage); //displays the message at the end of the html element(weather info)
 
 }
