2e526bcab82e86f2d2ff1ddb5e3745d86723acdf
mf2056 <mf2056@hw.ac.uk>
2023-11-24 23:43:28 +0400

    nodejs completed

---
+++ week5_nodejs/routes/pages.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req,res) => {
+    res.render('index');
+
+});
+
+router.get('/register', (req,res) => {
+    res.render('register');
+
+});
+
+router.get('/login', (req,res) => {
+    res.render('login');
+
+});
+
+router.get('/profile', (req,res) => {
+    const user = {
+        id: req.query.id,
+        name: req.query.name,
+        email: req.query.email
+    };
+    res.render('profile', { user });
+
+});
+
+module.exports = router;
